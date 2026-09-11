a2ec63bd9d85ce29bde5a546fe850b26b99eabd1
licstwitter-cpu <licstwitter@gmail.com>
2026-09-11 13:45:32 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyA7liGGYG20mu7jd6GoZKQKCRTRvDPQQcg",
-  authDomain: "chat-17634.firebaseapp.com",
-  databaseURL: "https://chat-17634-default-rtdb.firebaseio.com",
-  projectId: "chat-17634",
-  storageBucket: "chat-17634.firebasestorage.app",
-  messagingSenderId: "1045230573713",
-  appId: "1:1045230573713:web:2b71cf2ad7266b84a4c730"
-};
