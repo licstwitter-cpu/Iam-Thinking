33dfb79c01255e5986e6a05298d6d32e3bb14ce4
licstwitter-cpu <licstwitter@gmail.com>
2026-09-12 08:13:21 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD6aho_l4oyLFFPSe8-7xB1Tx9zfycdEAM",
-  authDomain: "red-social-muchos.firebaseapp.com",
-  databaseURL: "https://red-social-muchos-default-rtdb.firebaseio.com",
-  projectId: "red-social-muchos",
-  storageBucket: "red-social-muchos.firebasestorage.app",
-  messagingSenderId: "947442306337",
-  appId: "1:947442306337:web:b7901d251784a0294a0260"
-};
